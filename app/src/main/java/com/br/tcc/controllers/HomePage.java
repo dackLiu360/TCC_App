6c44329c0466fb25e2f62ac135848dd9dfaa2df6
Victor <vicmc14@gmail.com>
2018-01-12 05:13:35 -0200

    Quarto commit
    Mudei o layout do activity_home_page e activity_main para Relative layout
    
    Criei o side menu e adicionei somente na activity_main

--- app/src/main/java/com/br/tcc/controllers/HomePage.java
+++ app/src/main/java/com/br/tcc/controllers/HomePage.java
@@ -2,8 +2,14 @@ package com.br.tcc.controllers;
 
 import android.content.Intent;
 import android.database.Cursor;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -13,15 +19,30 @@ import com.example.victor.tcc.R;
 
 import java.util.ArrayList;
 
-public class HomePage extends AppCompatActivity {
+public class HomePage extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        NavigationView navigationView = findViewById(R.id.navMenuHome);
+
+        setContentView(R.layout.activity_home_page);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer);
+        mDrawerLayout.requestLayout();
+        mToggle = new ActionBarDrawerToggle(this,mDrawerLayout,R.string.open, R.string.close);
+        mDrawerLayout.addDrawerListener(mToggle);
+        mDrawerLayout.bringToFront();
+        setNavigationViewListener();
+        mToggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
 
         final UserDAO udao = new UserDAO(this);
         String username = null;
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home_page);
+
+
         Cursor data = udao.getData();
         while(data.moveToNext()){
 
@@ -35,15 +56,37 @@ public class HomePage extends AppCompatActivity {
         textoInicial.setText("Bem-vindo "+username);
 
 
-        buttonProfile.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+
+
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        if(mToggle.onOptionsItemSelected(item)){
+
+
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        System.out.println("aqui22");
+
+        switch (item.getItemId()) {
+
+            case R.id.buttonProfile:
                 Intent intent = new Intent(HomePage.this, Profile.class);
                 HomePage.this.startActivity(intent);
-            }
-        });
-
-
-
+        }
+        return true;
+    }
+    private void setNavigationViewListener() {
+        NavigationView navigationView = findViewById(R.id.navMenuHome);
+        System.out.println(navigationView.toString());
+        if (navigationView != null) {
+            navigationView.setNavigationItemSelectedListener(this);
+        }
     }
 }
